5b2ecacaf106575dbbe412d8b40a8f6496a34b67
bbidag <skawlgh112@gmail.com>
2021-05-30 17:56:33 +0900

    Initial commit

---
+++ src/main/java/com/bbidag/microservice/scm/SpringCloudMicroserviceScmApplication.java
@@ -0,0 +1,13 @@
+package com.bbidag.microservice.scm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMicroserviceScmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMicroserviceScmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbidag/microservice/scm/SpringCloudMicroserviceScmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbidag.microservice.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMicroserviceScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
