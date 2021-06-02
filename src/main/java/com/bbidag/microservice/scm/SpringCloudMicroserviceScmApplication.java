a5e29c1d17bc177824c7f8bf27980f11004c4f16
bbidag <skawlgh112@gmail.com>
2021-06-02 22:32:42 +0900

    feat: discovery client 테스트를 위한 서비스, 인스턴스 id 반환하는 api

--- src/main/java/com/bbidag/microservice/scm/SpringCloudMicroserviceScmApplication.java
+++ src/main/java/com/bbidag/microservice/scm/SpringCloudMicroserviceScmApplication.java
@@ -2,7 +2,9 @@ package com.bbidag.microservice.scm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudMicroserviceScmApplication {
 
